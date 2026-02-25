fba30465b9d1ef0737e2213cc1ee8c52d7914455
ydiouy <ydiouy@gmail.com>
2026-02-25 15:14:39 +0100

    Add environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  vacationApiUrl : 'https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-calendrier-scolaire/records'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  vacationApiUrl : 'https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-calendrier-scolaire/records'
+};
